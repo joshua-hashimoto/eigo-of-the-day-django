f1693943f96b99874b7b8cb4221a847b841d48d8
dropscar <dropscar041205@gmail.com>
2020-08-04 21:36:15 +0900

    updated models.py for eigo and migrated

--- eigo/models.py
+++ eigo/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from martor.models import MartorField
 
-# Create your models here.
+from core.additional.models import CoreModel
+
+
+class Phrase(CoreModel):
+    """
+    """
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    phrase = models.CharField(max_length=255)
+    memo = MartorField()
+
+
+class Example(CoreModel):
+    """
+    """
+    phrase = models.ForeignKey(
+        Phrase, on_delete=models.CASCADE, related_name='examples')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    example = models.TextField()
+    memo = MartorField()
+
+
+def upload_image_to(instance, filename):
+    asset_path = f'{str(instance.phrase.phrase)}/images/{filename}'
+    return asset_path
+
+
+class Snap(CoreModel):
+    """
+    """
+    phrase = models.ForeignKey(
+        Phrase, on_delete=models.CASCADE, related_name='snaps')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    snap = models.ImageField(upload_to=upload_image_to)
