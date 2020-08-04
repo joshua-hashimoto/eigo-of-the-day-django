1e22274fda3804fdb7f915c422a19f54b4718a16
dropscar <dropscar041205@gmail.com>
2020-08-04 21:36:23 +0900

    updated admin.py

--- eigo/admin.py
+++ eigo/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.db import models
+from martor.widgets import AdminMartorWidget
 
-# Register your models here.
+from .models import Phrase, Example, Snap
+
+
+def active(self, request, queryset):
+    queryset.update(is_active=True)
+
+
+active.short_description = '閲覧可能'
+
+
+def inactive(self, request, queryset):
+    queryset.update(is_active=False)
+
+
+inactive.short_description = '閲覧不可能'
+
+
+class ExampleInline(admin.TabularInline):
+    model = Example
+
+
+class SnapInline(admin.TabularInline):
+    model = Snap
+
+
+class PhraseAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
+
+    list_display = [
+        'id',
+        'phrase',
+        'is_active',
+    ]
+    list_display_links = [
+        'id',
+    ]
+    list_filter = [
+        'phrase',
+        'is_active',
+    ]
+    search_fields = [
+        'phrase',
+    ]
+    actions = [active, inactive]
+    inlines = [
+        ExampleInline,
+        SnapInline,
+    ]
+
+
+admin.site.register(Phrase, PhraseAdmin)
+admin.site.register(Example)
+admin.site.register(Snap)
