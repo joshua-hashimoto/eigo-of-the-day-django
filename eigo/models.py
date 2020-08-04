099684f07370075f1babebde4324ff5c2af60a02
dropscar <dropscar041205@gmail.com>
2020-08-05 01:44:09 +0900

    updated model

--- eigo/models.py
+++ eigo/models.py
@@ -9,8 +9,13 @@ class Phrase(CoreModel):
     """
     """
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    phrase = models.CharField(max_length=255)
-    memo = MartorField(blank=True)
+    phrase = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.phrase
+
+    def get_absolute_url(self):
+        return reverse("eigo:detail", kwargs={"pk": self.pk})
 
 
 class Example(CoreModel):
@@ -20,11 +25,13 @@ class Example(CoreModel):
         Phrase, on_delete=models.CASCADE, related_name='examples')
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     example = models.TextField()
-    memo = MartorField(blank=True)
+
+    def __str__(self):
+        return self.example
 
 
 def upload_image_to(instance, filename):
-    asset_path = f'{str(instance.phrase.phrase)}/images/{filename}'
+    asset_path = f'{str(instance.phrase.phrase)}/{filename}'
     return asset_path
 
 
@@ -35,3 +42,6 @@ class Snap(CoreModel):
         Phrase, on_delete=models.CASCADE, related_name='snaps')
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     snap = models.ImageField(upload_to=upload_image_to)
+
+    def __str__(self):
+        return self.snap.name
