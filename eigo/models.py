bec4da0f42fe60388ff45d46a29c481922b7e5da
dropscar <dropscar041205@gmail.com>
2020-08-05 23:24:29 +0900

    created QuerySet and Manager to model

--- eigo/models.py
+++ eigo/models.py
@@ -1,8 +1,42 @@
+from functools import reduce
+import operator
+import itertools
+
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Q
+from django.urls import reverse
 from martor.models import MartorField
 
 from core.additional.models import CoreModel
+
+
+class PhraseQueryset(models.QuerySet):
+
+    def all(self):
+        return self.filter(is_active=True)
+
+    def search(self, query):
+        query_list = [i.split(',') for i in query.split(' ')]
+        query_list = list(itertools.chain.from_iterable(query_list))
+        query_list = [i for i in query_list if i != '']
+        lookup = reduce(operator.or_, (Q(is_active=True) & Q(
+            phrase__icontains=option) for option in query_list))
+        return self.filter(lookup)
+
+
+class PhraseManager(models.Manager):
+
+    def get_queryset(self):
+        return PhraseQueryset(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().all()
+
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().search(query)
 
 
 class Phrase(CoreModel):
@@ -11,11 +45,13 @@ class Phrase(CoreModel):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     phrase = models.CharField(max_length=255, unique=True)
 
+    objects = PhraseManager()
+
     def __str__(self):
         return self.phrase
 
     def get_absolute_url(self):
-        return reverse("eigo:detail", kwargs={"pk": self.pk})
+        return reverse("eigo:eigo_detail", kwargs={"pk": self.pk})
 
 
 class Example(CoreModel):
