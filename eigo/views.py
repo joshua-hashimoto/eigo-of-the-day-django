125a702fa7e0ee188a4e0dad601034dfd956fc72
dropscar <dropscar041205@gmail.com>
2020-08-07 00:28:08 +0900

    changed views to use extra_views for create and update

--- eigo/views.py
+++ eigo/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import DeleteView
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy
+from extra_views import CreateWithInlinesView, UpdateWithInlinesView, NamedFormsetsMixin
 
-from .forms import PhraseForm, ExampleFormSet, SnapFormSet
+from .forms import ExampleInlineFormSet, SnapInlineFormSet
 from .models import Phrase
 
 
@@ -38,90 +39,35 @@ class PhraseDetailView(LoginRequiredMixin, DetailView):
     login_url = 'account_login'
 
 
-class PhraseCreateView(LoginRequiredMixin, CreateView):
+class PhraseCreateView(LoginRequiredMixin, NamedFormsetsMixin, CreateWithInlinesView):
     """
     """
     model = Phrase
-    template_name = 'eigo/eigo_form.html'
-    form_class = PhraseForm
+    inlines = [ExampleInlineFormSet, SnapInlineFormSet]
+    inlines_names = ['example_formset', 'snap_formset']
+    fields = ('phrase',)
+    template_name = 'eigo/eigo_new.html'
+    success_url = reverse_lazy('eigo:eigo_list')
     login_url = 'account_login'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.request.POST:
-            context['examples'] = ExampleFormSet(self.request.POST)
-            context['snaps'] = SnapFormSet(
-                self.request.POST, self.request.FILES)
-        else:
-            context['examples'] = ExampleFormSet()
-            context['snaps'] = SnapFormSet()
-        return context
-
     def form_valid(self, form):
-        context = self.get_context_data()
-        with transaction.atomic():
-            form.instance.user = self.request.user
-            self.object = form.save()
-        examples = context['examples']
-        if examples.is_valid():
-            examples.instance = self.object
-            presaved_examples = examples.save(commit=False)
-            for pre_example in presaved_examples:
-                pre_example.user = self.request.user
-                pre_example.save()
-        snaps = context['snaps']
-        if snaps.is_valid():
-            snaps.instance = self.object
-            presaved_snaps = snaps.save(commit=False)
-            for pre_snap in presaved_snaps:
-                pre_snap.user = self.request.user
-                pre_snap.save()
-        return super(PhraseCreateView, self).form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('eigo:eigo_detail', kwargs={'pk': self.object.pk})
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
-class PhraseUpdateView(LoginRequiredMixin, UpdateView):
+class PhraseUpdateView(LoginRequiredMixin, NamedFormsetsMixin, UpdateWithInlinesView):
     """
     """
     model = Phrase
-    template_name = 'eigo/eigo_form.html'
-    form_class = PhraseForm
+    inlines = [ExampleInlineFormSet, SnapInlineFormSet]
+    inlines_names = ['example_formset', 'snap_formset']
+    fields = ('phrase',)
+    template_name = 'eigo/eigo_edit.html'
     login_url = 'account_login'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.request.POST:
-            context['examples'] = ExampleFormSet(
-                self.request.POST, instance=self.object)
-            context['snaps'] = SnapFormSet(
-                self.request.POST, self.request.FILES, instance=self.object)
-        else:
-            context['examples'] = ExampleFormSet(instance=self.object)
-            context['snaps'] = SnapFormSet(instance=self.object)
-        return context
-
     def form_valid(self, form):
-        context = self.get_context_data()
-        with transaction.atomic():
-            form.instance.user = self.request.user
-            self.object = form.save()
-        examples = context['examples']
-        if examples.is_valid():
-            examples.instance = self.object
-            presaved_examples = examples.save(commit=False)
-            for pre_example in presaved_examples:
-                pre_example.user = self.request.user
-                pre_example.save()
-        snaps = context['snaps']
-        if snaps.is_valid():
-            snaps.instance = self.object
-            presaved_snaps = snaps.save(commit=False)
-            for pre_snap in presaved_snaps:
-                pre_snap.user = self.request.user
-                pre_snap.save()
-        return super(PhraseUpdateView, self).form_valid(form)
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy('eigo:eigo_detail', kwargs={'pk': self.object.pk})
