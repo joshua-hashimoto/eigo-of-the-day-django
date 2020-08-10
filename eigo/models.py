260eab4d093675ae7fe5cf4423c766d812e42d6c
dropscar <dropscar041205@gmail.com>
2020-08-10 14:40:27 +0900

    added comments to models.py

--- eigo/models.py
+++ eigo/models.py
@@ -12,11 +12,25 @@ from core.additional.models import CoreModel
 
 
 class PhraseQueryset(models.QuerySet):
+    """
+    custom queryset for model Phrase
+    """
 
     def all(self):
+        """
+        Returns:
+            queryset: return all object with is_active=True
+        """
         return self.filter(is_active=True)
 
     def search(self, query):
+        """
+        custom search for the model.
+        it works for search that include space ' ' and ',' in the search form.
+
+        Returns:
+            queryset: return filtered queryset using user input.
+        """
         query_list = [i.split(',') for i in query.split(' ')]
         query_list = list(itertools.chain.from_iterable(query_list))
         query_list = [i for i in query_list if i != '']
@@ -26,14 +40,39 @@ class PhraseQueryset(models.QuerySet):
 
 
 class PhraseManager(models.Manager):
+    """
+    custom manager for model Phrase using PhraseQuerySet.
+    """
 
     def get_queryset(self):
+        """
+        set custom queryset
+
+        Returns:
+            PhraseQueryset: return PhraseQueryset using model Phrase
+        """
         return PhraseQueryset(self.model, using=self._db)
 
     def all(self):
+        """
+        calls custom queryset's all() method
+
+        Returns:
+            queryset: return all object with is_active=True
+        """
         return self.get_queryset().all()
 
     def search(self, query=None):
+        """
+        if argument query is set to None, return .none().
+        if there is a value, pass it to custom queryset's search method.
+
+        Args:
+            query (str): user input query string
+
+        Returns:
+            queryset: return filtered queryset
+        """
         if query is None:
             return self.get_queryset().none()
         return self.get_queryset().search(query)
@@ -41,6 +80,12 @@ class PhraseManager(models.Manager):
 
 class Phrase(CoreModel):
     """
+    model to save Phrases
+
+    Attributes:
+        user (ForeignKey): one-to-one relation to set user to model
+        phrase (CharField): field to save the actual phrase. max length to 255 charactors
+        objects (PhraseManager): set custom Manager to model
     """
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     phrase = models.CharField(max_length=255, unique=True)
@@ -48,32 +93,75 @@ class Phrase(CoreModel):
     objects = PhraseManager()
 
     class Meta:
+        """
+        Attributes:
+            ordering (List): use to determine the ordering of model objects when listed
+        """
         ordering = ['-timestamp', '-updated', ]
 
     def __str__(self):
+        """
+        determine which field of the model should be representing the model object.
+        mainly used in admin site.
+
+        Returns:
+            str: returns phrase field.
+        """
         return self.phrase
 
     def get_absolute_url(self):
+        """
+        determine to absolute url of the model.
+        mainly used to call each object through urls in template.
+        """
         return reverse("eigo:eigo_detail", kwargs={"pk": self.pk})
 
 
 class ExampleQueryset(models.QuerySet):
+    """
+    custom queryset for model Example
+    """
 
     def all(self):
+        """
+        Returns:
+            queryset: return all object with is_active=True
+        """
         return self.filter(is_active=True)
 
 
 class ExampleManager(models.Manager):
+    """
+    custom manager for model Example using ExampleQuerySet.
+    """
 
     def get_queryset(self):
+        """
+        set custom queryset
+
+        Returns:
+            ExampleQueryset: return ExampleQueryset using model Example
+        """
         return ExampleQueryset(self.model, using=self._db)
 
     def all(self):
+        """
+        calls custom queryset's all() method
+
+        Returns:
+            queryset: return all object with is_active=True
+        """
         return self.get_queryset().all()
 
 
 class Example(CoreModel):
     """
+    model to save Example
+
+    Attributes:
+        phrase (ForeignKey): one-to-one relation to set phrase to model
+        example (TextField): field to save the actual phrase
+        objects (ExampleManager): set custom Manager to model
     """
     phrase = models.ForeignKey(
         Phrase, on_delete=models.CASCADE, related_name='examples')
@@ -82,21 +170,50 @@ class Example(CoreModel):
     objects = ExampleManager()
 
     def __str__(self):
+        """
+        determine which field of the model should be representing the model object.
+        mainly used in admin site.
+
+        Returns:
+            str: returns example field.
+        """
         return self.example
 
 
 class SnapQueryset(models.QuerySet):
+    """
+    custom queryset for model Snap
+    """
 
     def all(self):
+        """
+        Returns:
+            queryset: return all object with is_active=True
+        """
         return self.filter(is_active=True)
 
 
 class SnapManager(models.Manager):
+    """
+    custom manager for model Snap using SnapQuerySet.
+    """
 
     def get_queryset(self):
+        """
+        set custom queryset
+
+        Returns:
+            SnapQueryset: return SnapQueryset using model Snap
+        """
         return SnapQueryset(self.model, using=self._db)
 
     def all(self):
+        """
+        calls custom queryset's all() method
+
+        Returns:
+            queryset: return all object with is_active=True
+        """
         return self.get_queryset().all()
 
 
@@ -107,6 +224,12 @@ def upload_image_to(instance, filename):
 
 class Snap(CoreModel):
     """
+    model to save Snap
+
+    Attributes:
+        phrase (ForeignKey): one-to-one relation to set phrase to model
+        snap (ImageField): field to save the actual phrase
+        objects (SnapManager): set custom Manager to model
     """
     phrase = models.ForeignKey(
         Phrase, on_delete=models.CASCADE, related_name='snaps')
@@ -115,4 +238,11 @@ class Snap(CoreModel):
     objects = SnapManager()
 
     def __str__(self):
+        """
+        determine which field of the model should be representing the model object.
+        mainly used in admin site.
+
+        Returns:
+            str: returns name of the snap field.
+        """
         return self.snap.name
